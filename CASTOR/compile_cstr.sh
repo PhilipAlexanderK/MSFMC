97269473d46956ee6c43706bab451be4fca13742
pkala <36844065+PhilipAlexanderK@users.noreply.github.com>
2025-02-21 15:14:19 +0100

    Update compile_cstr.sh

--- CASTOR/compile_cstr.sh
+++ CASTOR/compile_cstr.sh
@@ -3,5 +3,14 @@
 # Compile CASToR.
 unzip castor_v3.2.1.zip
 
+cd castor_v3.2
+
+export CASTOR_CONFIG=/home/vgate/MSFMC/CASTOR/castor_v3.2/config/
+export CASTOR_ROOT=1
+
+make
+
+echo 'export PATH=$PATH:/home/vgate/MSFMC/CASTOR/castor_v3.2/bin' >> /home/vgate/.bashrc
+
 
 
